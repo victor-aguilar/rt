6247a48a727076eede19a946501668b1a086d7c1
jose.aguilar <jose.aguilar@smg>
2012-08-17 15:35:48 -0600

    Se limpio el archiov configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -5,13 +5,6 @@ define("USER_DB", "rt");
 define("PASSWORD_DB","r2d2");
 define("DB_NAME","tutorias");
 
-define(
-        "DIRECTORIO_BASE", 
-        "archivosSubidos/");
-define("DIR" , "victor/chat/");
-
-define("DIRECTORIO_BASE_WEB","x");
-
 date_default_timezone_set("America/Mexico_City");
 
 function dameConexion(){
